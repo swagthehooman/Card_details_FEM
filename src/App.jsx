2a2b51db98831a81e2be7a3a20590ff452fb90da
Swagnik Das <104345028+swagthehooman@users.noreply.github.com>
2023-03-25 00:14:02 +0530

    Input validation completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,106 @@
-import React, {useState,useEffect} from 'react'
-import CardForm from './components/CardForm'
-import CardFront from './components/CardFront'
-import CardBack from './components/CardBack'
-
+import React, { useState, useEffect } from "react";
+import CardForm from "./components/CardForm";
+import CardFront from "./components/CardFront";
+import CardBack from "./components/CardBack";
 
 function App() {
+  const cardRegex = new RegExp("^[0-9]{16}$");
+  const cvvRegex = new RegExp("^[1-9][0-9]{2}$");
+  const monthRegex = new RegExp("(^0[1-9]$)|(^1[0-2]$)");
+  const yearRegex = new RegExp("(^0[1-9]$)|(^[1-9]{2}$)");
 
-  const [cardInfo, setCardInfo]=useState({
-    'cardName':'',
-    'cardNumber':'',
-    'month':'',
-    'year':'',
-    'cvv':'',
-  })
-  const [isValid, setIsValid]=useState({
-    'numberFormat':false,
-    'nullInput':false,
-    'inputLength':false
-  })
+  const [cardInfo, setCardInfo] = useState({
+    cardName: "",
+    cardNumber: "",
+    month: "",
+    year: "",
+    cvv: "",
+  });
+  const [isValid, setIsValid] = useState({
+    cardNumberValid: false,
+    monthValid: false,
+    yearValid: false,
+    cvvValid: false,
+  });
 
-  function handleChange(event){
-      const {name,value}=event.target
+  function handleChange(event) {
+    const { name, value } = event.target;
 
-      setCardInfo(prev=>({
-        ...prev,
-        [name]:value
-      }))
+    setCardInfo((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
   }
 
-  function handleConfirm(event){
+  function handleConfirm(event) {
+    setConfirm((prev) => !prev);
+    var number = cardInfo.cardNumber;
+    var month = cardInfo.month;
+    var year = cardInfo.year;
+    var cvv = cardInfo.cvv;
 
-    var cardNumber=cardInfo.cardNumber
-    var cardName=cardInfo.cardName
-    var cvv=cardInfo.cvv
-    var mon=cardInfo.month
-    var yr=cardInfo.year
-
-    let temp=''
-
-    for(let i=0;i<16;i++){
-        if(i%4==0 && i!==0)
-          temp+=' '
-        else
-          temp+=cardName.charAt(i)
+    if (cardRegex.test(number)) {
+      setIsValid((prev) => ({
+        ...prev,
+        cardNumberValid: true,
+      }));
+    } else {
+      setIsValid((prev) => ({
+        ...prev,
+        cardNumberValid: false,
+      }));
     }
-
-      cardName=temp
-      console.log(cardName)
-      // if(cardName===''||cardNumber===''|| cvv===''|| mon===''|| yr===''){
-      //   setIsValid(prev=>({
-      //     ...prev,
-      //     'nullInput':true
-      //   }))
-      // }
-      // if(cardNumber.length!==19 || cvv.length!==3 || mon.length!==2 || yr.length!==2){
-      //   setIsValid(prev=>({
-      //     ...prev,
-      //     'inputLength':true
-      //   }))
-      // if()
-      // }
+    if (monthRegex.test(month)) {
+      setIsValid((prev) => ({
+        ...prev,
+        monthValid: true,
+      }));
+    } else {
+      setIsValid((prev) => ({
+        ...prev,
+        monthValid: false,
+      }));
+    }
+    if (yearRegex.test(year)) {
+      setIsValid((prev) => ({
+        ...prev,
+        yearValid: true,
+      }));
+    } else {
+      setIsValid((prev) => ({
+        ...prev,
+        yearValid: false,
+      }));
+    }
+    if (cvvRegex.test(cvv)) {
+      setIsValid((prev) => ({
+        ...prev,
+        cvvValid: true,
+      }));
+    } else {
+      setIsValid((prev) => ({
+        ...prev,
+        cvvValid: false,
+      }));
+    }
+    console.log(isValid);
   }
   //use for changing the form confirmation looks and other things
-  
 
-    return (
-      <main>
-        <div className='card-holder'>
-          <CardFront
-            cardFrontInfo={cardInfo}
-            />
-          <CardBack cardBackInfo={cardInfo}/>
-        </div>
-          <CardForm handleChange={handleChange} 
-            cardInfo={cardInfo}
-            confirm={handleConfirm}
-            />
-
-      </main>
-    )
+  return (
+    <main>
+      <div className="card-holder">
+        <CardFront cardFrontInfo={cardInfo} />
+        <CardBack cardBackInfo={cardInfo} />
+      </div>
+      <CardForm
+        handleChange={handleChange}
+        cardInfo={cardInfo}
+        confirm={handleConfirm}
+        validation={isValid}
+      />
+    </main>
+  );
 }
 
-export default App
+export default App;
--- src/components/CardForm.jsx
+++ src/components/CardForm.jsx
@@ -1,37 +1,78 @@
 import React from "react";
 
-export default function CardForm(props){
-    return (
-        <div className="input-form">
-        <div className="form-container">
-            <div className="cardholder-info">
-                <label>CARDHOLDER NAME</label>
-                <input type="text" placeholder="e.g. Jane Appleseed" 
-                    name="cardName" onChange={props.handleChange} value={props.cardInfo.cardName}/>
-            </div>
-            <div className="cardholder-info">
-                <label>CARD NUMBER</label>
-                <input type="text" placeholder="e.g. 1234 5678 9123 0000"
-                    name="cardNumber" onChange={props.handleChange} value={props.cardInfo.cardNumber}/>
-            </div>
-            <div className="exp-cvv-date">
-                    <div className="exp-date">
-                        <label>EXP. DATE (MM/YY)</label>
-                        <div className="date">
-                            <input type="text" placeholder="MM" name="month" 
-                                onChange={props.handleChange} value={props.cardInfo.month}/>
-                            <input type="text" placeholder="YY" name="year" 
-                                onChange={props.handleChange} value={props.cardInfo.year}/>
-                        </div>
-                    </div>
-                    <div className="cvv">
-                        <label>CVC</label>
-                        <input type="text" placeholder="e.g. 123" name="cvv" 
-                            onChange={props.handleChange} value={props.cardInfo.cvv}/>
-                    </div>
-            </div>
-            <button onClick={props.confirm}>Confirm</button>
+export default function CardForm(props) {
+  return (
+    <div className="input-form">
+      <div className="form-container">
+        <div className="cardholder-info">
+          <label>CARDHOLDER NAME</label>
+          <input
+            type="text"
+            placeholder="e.g. Jane Appleseed"
+            name="cardName"
+            onChange={props.handleChange}
+            value={props.cardInfo.cardName}
+          />
         </div>
+        <div className="cardholder-info">
+          <label>CARD NUMBER</label>
+          <input
+            type="text"
+            placeholder="e.g. 1234 5678 9123 0000"
+            name="cardNumber"
+            onChange={props.handleChange}
+            value={props.cardInfo.cardNumber}
+          />
+          {!props.validation.cardNumberValid && (
+            <p className="error">Wrong format, numbers only.</p>
+          )}
         </div>
-    )
+        <div className="exp-cvv-date">
+          <div className="exp-date">
+            <label>EXP. DATE (MM/YY)</label>
+            <div className="date">
+              <div>
+                <input
+                  type="text"
+                  placeholder="MM"
+                  name="month"
+                  onChange={props.handleChange}
+                  value={props.cardInfo.month}
+                />
+                {!props.validation.monthValid && (
+                  <p className="error">Wrong format.</p>
+                )}
+              </div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="YY"
+                  name="year"
+                  onChange={props.handleChange}
+                  value={props.cardInfo.year}
+                />
+                {!props.validation.yearValid && (
+                  <p className="error">Wrong format.</p>
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="cvv">
+            <label>CVV</label>
+            <input
+              type="text"
+              placeholder="e.g. 123"
+              name="cvv"
+              onChange={props.handleChange}
+              value={props.cardInfo.cvv}
+            />
+            {!props.validation.cvvValid && (
+              <p className="error">Wrong format.</p>
+            )}
+          </div>
+        </div>
+        <button onClick={props.confirm}>Confirm</button>
+      </div>
+    </div>
+  );
 }
