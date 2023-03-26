3eaaaf81056f0361197c962b4ce04ef326e26454
Swagnik Das <104345028+swagthehooman@users.noreply.github.com>
2023-03-26 12:04:52 +0530

    project completion

--- src/components/Completion.jsx
+++ src/components/Completion.jsx
@@ -3,13 +3,13 @@ import React from "react";
 export default function Completion() {
   return (
     <div className="input-form">
-      <div className="completion">
+      <div className="completion form-container">
         <img
           className="complete-img"
           src="./images/icon-complete.svg"
           alt="tick"
         />
-        <h1>THANK YOU</h1>
+        <h1>THANK YOU!</h1>
         <p className="details">We've added your card details</p>
         <button>Continue</button>
       </div>
